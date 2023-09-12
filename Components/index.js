78166d8c36fc50c896aa485fb09163fd28093f34
panditdhamdhere <dhamdherepandit@gmail.com>
2023-09-12 11:20:33 +0530

    component import and exported

---
+++ Components/index.js
@@ -0,0 +1,35 @@
+import About from "./About";
+import ArrowUp from "./ArrowUp";
+import Banner from "./Banner";
+import Blog from "./Blog";
+import Client from "./Client";
+import Contact from "./Contact";
+import Distribution from "./Distributation";
+import Faq from "./Faq";
+import Footer from "./Footer";
+import Header from "./Header";
+import Loader from "./Loader";
+import MobileApp from "./MobileApp";
+import Service from "./Service";
+import Team from "./Team";
+import Timeline from "./Timeline";
+import Tokensale from "./Tokensale";
+
+export {
+  About,
+  ArrowUp,
+  Banner,
+  Blog,
+  Client,
+  Contact,
+  Distribution,
+  Faq,
+  Footer,
+  Header,
+  Loader,
+  MobileApp,
+  Service,
+  Team,
+  Timeline,
+  Tokensale,
+};
