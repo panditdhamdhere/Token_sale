cb0bef82d2e3a19418de0b1e4ae2c55b0379dffd
panditdhamdhere <dhamdherepandit@gmail.com>
2023-09-13 08:08:30 +0530

    fixed scripts and banner added

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,12 @@ export default function App({ Component, pageProps }) {
       </StateContextProvider>
 
       <script src="assets/js/jquery-1.12.4.min.js"></script>
-      <script src="assets/bootstarp/bootstrap.min.js"></script>
+      <script src="assets/bootstarp/js/bootstrap.min.js"></script>
       <script src="assets/owlcarousel/js/owl.carousel.min.js"></script>
       <script src="assets/js/magnific-popup.min.js"></script>
       <script src="assets/js/waypoints.min.js"></script>
       <script src="assets/js/parallax.min.js"></script>
-      <script src="assets/js/jquery.counterup.min.js"></script>
+      <script src="assets/js/jquery.countdown.min.js"></script>
       <script src="assets/js/particles.min.js"></script>
       <script src="assets/js/jquery.dd.min.js"></script>
       <script src="assets/js/jquery.counterup.min.js"></script>
